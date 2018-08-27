adba94142fba75c6d1db8237ade1d2efbdbfc561
Damh119212 <36243830+Damh119212@users.noreply.github.com>
2018-08-27 07:54:47 -0500

    Update proxypub.py

--- Modulo/proxypub.py
+++ Modulo/proxypub.py
@@ -17,7 +17,7 @@ import traceback
 if sys.argv[2:]:
  msg1 = sys.argv[2]
 else:
- msg1 = "ADM-ULTIMATE"
+ msg1 = "<font color="red">PROXY SOCKS</font> <font color="blue">#@DarksideBlack</font>"
 
 
 class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
